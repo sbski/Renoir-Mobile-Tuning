964dd8a5b4564aec14136b1049c73233d959968b
sbski <37050207+sbski@users.noreply.github.com>
2020-08-09 09:40:47 -0500

    recreating project

---
+++ RenoirMobileTuning/RenoirMobileTuning/RenoirMobileTuning/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenLibSys;
+using System.Threading;
+
+namespace RenoirMobileTuning
+{
+    public partial class rmtForm : Form
+    {
+        private readonly Ols rmtOls;
+        readonly Mutex hMutexPci;
+
+        private uint SMU_ADDR_MSG;
+        private uint SMU_ADDR_RSP;
+        private uint SMU_ADDR_ARG;
+
+        public rmtForm()
+        {
+            InitializeComponent();
+
+            rmtOls = new Ols();
+            hMutexPci = new Mutex();
+            try
+            {
+                CheckOlsStatus();
+            }
+            catch (ApplicationException ex)
+            {
+                MessageBox.Show(ex.Message);
+                Dispose();
+                Application.Exit();
+            }
+        }
+
+        private void CheckOlsStatus()
+        {
+            // Check support library status
+            switch (ols.GetStatus())
+            {
+                case (uint)Ols.Status.NO_ERROR:
+                    break;
+                case (uint)Ols.Status.DLL_NOT_FOUND:
+                    throw new ApplicationException("WinRing DLL_NOT_FOUND");
+                case (uint)Ols.Status.DLL_INCORRECT_VERSION:
+                    throw new ApplicationException("WinRing DLL_INCORRECT_VERSION");
+                case (uint)Ols.Status.DLL_INITIALIZE_ERROR:
+                    throw new ApplicationException("WinRing DLL_INITIALIZE_ERROR");
+            }
+
+            // Check WinRing0 status
+            switch (ols.GetDllStatus())
+            {
+                case (uint)Ols.OlsDllStatus.OLS_DLL_NO_ERROR:
+                    break;
+                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED:
+                    throw new ApplicationException("WinRing OLS_DRIVER_NOT_LOADED");
+                case (uint)Ols.OlsDllStatus.OLS_DLL_UNSUPPORTED_PLATFORM:
+                    throw new ApplicationException("WinRing OLS_UNSUPPORTED_PLATFORM");
+                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_FOUND:
+                    throw new ApplicationException("WinRing OLS_DLL_DRIVER_NOT_FOUND");
+                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_UNLOADED:
+                    throw new ApplicationException("WinRing OLS_DLL_DRIVER_UNLOADED");
+                case (uint)Ols.OlsDllStatus.OLS_DLL_DRIVER_NOT_LOADED_ON_NETWORK:
+                    throw new ApplicationException("WinRing DRIVER_NOT_LOADED_ON_NETWORK");
+                case (uint)Ols.OlsDllStatus.OLS_DLL_UNKNOWN_ERROR:
+                    throw new ApplicationException("WinRing OLS_DLL_UNKNOWN_ERROR");
+            }
+        }
+
+    }
+}
---
+++ RenoirMobileTuning/RenoirMobileTuning/RenoirMobileTuning/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RenoirMobileTuning
+{
+    static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new rmtForm());
+        }
+    }
+}
